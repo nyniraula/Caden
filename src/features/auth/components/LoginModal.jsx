27b41807fd5dd202bbd4221873302aae81e15325
nyniraula <nyniraula@gmail.com>
2026-06-03 11:57:20 +0545

    Scaffold project

---
+++ src/features/auth/components/LoginModal.jsx
@@ -0,0 +1,5 @@
+const LoginModal = () => {
+  return <div className="max-w-md">LoginModal</div>;
+};
+
+export default LoginModal;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import LoginModal from "../features/auth/components/LoginModal";
+
+const Login = () => {
+  return (
+    <div>
+      <LoginModal />
+    </div>
+  );
+};
+
+export default Login;
